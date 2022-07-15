11a473484c47c77278b0e500e7d5de4e589c62e6
Felipe Aburto <aburtof222@gmail.com>
2022-07-15 00:52:43 -0600

    Se agregaron tablas y relaciones a la base de datos

--- database/restapi_nodejs_express.sql
+++ database/restapi_nodejs_express.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-07-2022 a las 08:06:14
+-- Tiempo de generación: 15-07-2022 a las 08:25:56
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -18,30 +18,63 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `restapi_nodejs_express`
+-- Base de datos: `mitienda`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `language`
+-- Estructura de tabla para la tabla `categorias`
 --
 
-CREATE TABLE `language` (
-  `id` tinyint(2) UNSIGNED NOT NULL,
-  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `programmers` tinyint(2) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Store the programming languages.';
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `descripcion` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
--- Volcado de datos para la tabla `language`
+-- Estructura de tabla para la tabla `clientes`
 --
 
-INSERT INTO `language` (`id`, `name`, `programmers`) VALUES
-(1, 'JavaScript', 12),
-(2, 'Python', 21),
-(4, 'PHP', 50),
-(6, 'C# .NET', 45);
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `direcciones_id` int(11) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ventas`
+--
+
+CREATE TABLE `detalle_ventas` (
+  `id` int(11) NOT NULL,
+  `ventas_id` int(11) NOT NULL,
+  `productos_id` int(11) NOT NULL,
+  `precio` float NOT NULL,
+  `descuento` float NOT NULL,
+  `total` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direcciones`
+--
+
+CREATE TABLE `direcciones` (
+  `id` int(11) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `calle` varchar(45) NOT NULL,
+  `colonia` varchar(45) NOT NULL,
+  `ciudad` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -53,18 +86,33 @@ CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `precio` double NOT NULL,
-  `stock` int(11) NOT NULL
+  `stock` int(11) NOT NULL,
+  `categorias_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `nombre`, `precio`, `stock`) VALUES
-(1, 'procesador intel core i7 8va gen', 7000, 10),
-(2, 'tarjeta madre atx mod 1800', 2000, 15),
-(3, 'teclado inalambrico', 1500, 30),
-(4, 'mouse gamer 12000 ppi', 2000, 10);
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `stock`, `categorias_id`) VALUES
+(1, 'procesador intel core i7 8va gen', 7000, 10, NULL),
+(2, 'tarjeta madre atx mod 1800', 2000, 15, NULL),
+(3, 'teclado inalambrico', 1500, 30, NULL),
+(4, 'mouse gamer 12000 ppi', 2000, 10, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `direcciones_id` int(11) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `correo` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -87,21 +135,64 @@ INSERT INTO `usuarios` (`id`, `usuario`, `contraseña`) VALUES
 (2, 'luis', 'luis'),
 (3, 'tomas', 'tomas');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `cliente` varchar(45) NOT NULL,
+  `cant_articulos` int(11) NOT NULL,
+  `subtotal` float NOT NULL,
+  `descuento` float NOT NULL,
+  `total` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `language`
+-- Indices de la tabla `categorias`
 --
-ALTER TABLE `language`
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `direcciones_id` (`direcciones_id`);
+
+--
+-- Indices de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD KEY `ventas_id` (`ventas_id`);
+
+--
+-- Indices de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorias_id` (`categorias_id`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `direcciones_id` (`direcciones_id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -110,14 +201,14 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indices de la tabla `ventas`
 --
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de la tabla `language`
+-- AUTO_INCREMENT de las tablas volcadas
 --
-ALTER TABLE `language`
-  MODIFY `id` tinyint(2) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -130,6 +221,34 @@ ALTER TABLE `productos`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`direcciones_id`) REFERENCES `direcciones` (`id`);
+
+--
+-- Filtros para la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD CONSTRAINT `detalle_ventas_ibfk_1` FOREIGN KEY (`ventas_id`) REFERENCES `ventas` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categorias_id`) REFERENCES `categorias` (`id`);
+
+--
+-- Filtros para la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD CONSTRAINT `proveedores_ibfk_1` FOREIGN KEY (`direcciones_id`) REFERENCES `direcciones` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
